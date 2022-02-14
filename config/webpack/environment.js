83ddf4c105fc9fccb44f8aadbedccd815ba82ca3
EC2 Default User <ec2-user@ip-172-31-17-86.ec2.internal>
2022-02-14 12:31:48 +0000

    [Add] 4

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,7 +7,7 @@ environment.plugins.prepend(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    JQuery: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
   })
-  )
+)
